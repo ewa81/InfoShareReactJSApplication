29a9b9c6711bf112cb74a41b36723a010e621b23
ewa81 <ewa.strzelecka@icloud.com>
2020-07-13 23:44:05 +0200

    Fixes and removed.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,30 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Todos from "./Components/Todos";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row, Col } from "react-bootstrap";
+import {Container, Row, Col} from "react-bootstrap";
 import Nav from "./Components/Nav";
 import TodoForm from "./Components/TodoForm";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return (
-      <Router>
-        <div className="todosApp">
-          <Container>
-            <Nav />
-            <Row>
-              <Col>
-                <Switch>
-                  <Redirect exact="exact" from="/" to="/todos" />
-                  <Route path="/todos" to="/todos" component={Todos} />
-                  <Route path="/form" component={TodoForm} />
-                </Switch>
-                <Todos />
-              </Col>
-            </Row>
-          </Container>
-        </div>
-      </Router>
-    );
+    return (<Router>
+      <div className="todosApp">
+        <Container>
+          <Nav/>
+          <Row>
+            <Col>
+              <Switch>
+                <Redirect exact="exact" from="/" to="/todos"/>
+                <Route path="/todos" to="/todos" component={Todos}/>
+                <Route path="/form" component={TodoForm}/>
+              </Switch>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </Router>);
   }
 }
 
