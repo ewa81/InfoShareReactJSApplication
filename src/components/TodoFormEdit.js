32853e53929f39dd0d423c467914e362c2a0e7f3
ewa81 <ewa.strzelecka@icloud.com>
2020-09-03 21:46:37 +0200

    Add bem.

--- src/components/TodoFormEdit.js
+++ src/components/TodoFormEdit.js
@@ -3,8 +3,8 @@ import React, { Component } from "react";
 class TodoFormEdit extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello, TodoFormEdit Component here, this, is an id you have sent: {this.props.match.params.id}</h1>
+      <div className="todosFormEdit">
+        <h1 className="todosFormEdit__id">Hello, TodoFormEdit Component here, this, is an id you have sent: {this.props.match.params.id}</h1>
       </div>
     );
   };
