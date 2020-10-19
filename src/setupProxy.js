7a5908d17c110d5393ec4e8a5c5cba2a7e31a67d
ewa81 <ewa.strzelecka@icloud.com>
2020-10-19 23:09:53 +0200

    Movie setupProxy.js to src and fixes change variable.

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
-const proxy = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function(app) {
-  app.use(proxy("/api/todos", {
+  app.use(createProxyMiddleware("/api/todos", {
     target: "http://localhost:8080",
     secure: false,
     changeOrigin: true
