889301535938de3460eb5330b902c116f4b1a49f
ewa81 <ewa.strzelecka@icloud.com>
2021-05-26 00:04:00 +0200

    Layout grid, color menu, add FontAwesomeIcon.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,32 @@
 import React, {Component} from "react";
 import {Link} from "react-router-dom";
 import {Row, Col} from "react-bootstrap";
+import "./Nav.scss";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPlus} from "@fortawesome/free-solid-svg-icons";
 
 class Nav extends Component {
   render() {
     return (
-      <Row>
+      <Row className="todosNav__menu mb-3">
         <Col className="todosNav" sm={12}>
           <nav className="todosNav__nav">
-            <ul className="todosNav__list">
-              <Link to="/todos">
-                <li>Todos</li>
-              </Link>
-              <Link to="/form">
-            <li>Form</li>
-          </Link>
-        </ul>
-      </nav>
-    </Col>
-  </Row>);
+            <Row className="todosNav__list m-2 p-0 d-flex justify-content-between" as="ul">
+              <Col>
+                <Link to="/todos">
+                  <li className="todosNav__link p-2">Todos</li>
+                </Link>
+              </Col>
+              <Col className="d-flex justify-content-end">
+                <Link to="/form">
+                  <li className="todosNav__icon p-2"><FontAwesomeIcon className="todos__icon todos__icon--plus" icon={faPlus}/></li>
+                </Link>
+              </Col>
+            </Row>
+          </nav>
+        </Col>
+      </Row>
+    );
   }
 }
 
