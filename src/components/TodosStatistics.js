ff3a5d9be4f771a0664f8323ab99298bf605f977
ewa81 <ewa.strzelecka@icloud.com>
2021-05-30 20:04:10 +0200

    Badge layout with statistic.

--- src/components/TodosStatistics.js
+++ src/components/TodosStatistics.js
@@ -1,19 +1,26 @@
 import React, { Component } from "react";
 import "./TodosStatistics.scss";
+import {Row, Col} from "react-bootstrap";
 
 class TodosStatistics extends Component {
   render() {
     return (
       <div className="todosStatistics">
-        <p className="todosStatistics__statusCount todosStatistics__statusCount--done p-4 rounded">
-          DONE: {this.getTodosCountByStatus("DONE")}
-        </p>
-        <p className="todosStatistics__statusCount todosStatistics__statusCount--inprogress p-4 rounded">
-          IN_PROGRESS: {this.getTodosCountByStatus("IN_PROGRESS")}
-        </p>
-        <p className="todosStatistics__statusCount todosStatistics__statusCount--todo p-4 rounded">
-          TODO: {this.getTodosCountByStatus("TODO")}
-        </p>
+          <Col className="p-0">
+            <p className="todosStatistics__statusCount todosStatistics__statusCount--done mb-2 p-4 rounded d-flex justify-content-between">
+              DONE<span className="badge badge-light text-success p-2">{this.getTodosCountByStatus("DONE")}</span>
+            </p>
+          </Col>
+          <Col className="p-0">
+            <p className="todosStatistics__statusCount todosStatistics__statusCount--inprogress mb-2 p-4 rounded d-flex justify-content-between">
+              IN_PROGRESS<span className="badge badge-light text-warning p-2">{this.getTodosCountByStatus("IN_PROGRESS")}</span>
+            </p>
+          </Col>
+          <Col className="p-0">
+            <p className="todosStatistics__statusCount todosStatistics__statusCount--todo mb-2 p-4 rounded d-flex justify-content-between">
+              TODO<span className="badge badge-light text-danger p-2">{this.getTodosCountByStatus("TODO")}</span>
+            </p>
+          </Col>
       </div>
     );
   }
