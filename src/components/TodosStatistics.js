2c41b8ff58cdff6e67323ed2e8bcdde348d9730f
ewa81 <ewa.strzelecka@icloud.com>
2021-05-31 23:00:16 +0200

    Done.

--- src/components/TodosStatistics.js
+++ src/components/TodosStatistics.js
@@ -8,17 +8,23 @@ class TodosStatistics extends Component {
       <div className="todosStatistics">
           <Col className="p-0">
             <p className="todosStatistics__statusCount todosStatistics__statusCount--done mb-2 p-3 rounded d-flex justify-content-between align-items-center">
-              DONE<span className="todosStatistics__statusDone p-2 rounded">{this.getTodosCountByStatus("DONE")}</span>
+              <span className="todosStatistics__statusLabel">DONE</span>
+              <span className="todosStatistics__statusDone p-2 rounded">
+                {this.getTodosCountByStatus("DONE")}</span>
             </p>
           </Col>
           <Col className="p-0">
             <p className="todosStatistics__statusCount todosStatistics__statusCount--inprogress mb-2 p-3 rounded d-flex justify-content-between align-items-center">
-              IN_PROGRESS<span className="todosStatistics__statusInprogress p-2 rounded">{this.getTodosCountByStatus("IN_PROGRESS")}</span>
+              <span className="todosStatistics__statusLabel">IN_PROGRESS</span>
+              <span className="todosStatistics__statusInprogress p-2 rounded">
+                {this.getTodosCountByStatus("IN_PROGRESS")}</span>
             </p>
           </Col>
           <Col className="p-0">
             <p className="todosStatistics__statusCount todosStatistics__statusCount--todo mb-2 p-3 rounded d-flex justify-content-between align-items-center">
-              TODO<span className="todosStatistics__statusTodo p-2 rounded">{this.getTodosCountByStatus("TODO")}</span>
+              <span className="todosStatistics__statusLabel">TODO</span>
+              <span className="todosStatistics__statusTodo p-2 rounded">
+                {this.getTodosCountByStatus("TODO")}</span>
             </p>
           </Col>
       </div>
