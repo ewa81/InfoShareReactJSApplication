b5f70324f1d627b71db5611971a4352763a9dadb
ewa81 <ewa.strzelecka@icloud.com>
2021-05-31 20:45:33 +0200

    Final one.

--- src/components/TodosStatistics.js
+++ src/components/TodosStatistics.js
@@ -7,18 +7,18 @@ class TodosStatistics extends Component {
     return (
       <div className="todosStatistics">
           <Col className="p-0">
-            <p className="todosStatistics__statusCount todosStatistics__statusCount--done mb-2 p-4 rounded d-flex justify-content-between">
-              DONE<span className="badge badge-light text-success p-2">{this.getTodosCountByStatus("DONE")}</span>
+            <p className="todosStatistics__statusCount todosStatistics__statusCount--done mb-2 p-3 rounded d-flex justify-content-between align-items-center">
+              DONE<span className="todosStatistics__statusDone p-2 rounded">{this.getTodosCountByStatus("DONE")}</span>
             </p>
           </Col>
           <Col className="p-0">
-            <p className="todosStatistics__statusCount todosStatistics__statusCount--inprogress mb-2 p-4 rounded d-flex justify-content-between">
-              IN_PROGRESS<span className="badge badge-light text-warning p-2">{this.getTodosCountByStatus("IN_PROGRESS")}</span>
+            <p className="todosStatistics__statusCount todosStatistics__statusCount--inprogress mb-2 p-3 rounded d-flex justify-content-between align-items-center">
+              IN_PROGRESS<span className="todosStatistics__statusInprogress p-2 rounded">{this.getTodosCountByStatus("IN_PROGRESS")}</span>
             </p>
           </Col>
           <Col className="p-0">
-            <p className="todosStatistics__statusCount todosStatistics__statusCount--todo mb-2 p-4 rounded d-flex justify-content-between">
-              TODO<span className="badge badge-light text-danger p-2">{this.getTodosCountByStatus("TODO")}</span>
+            <p className="todosStatistics__statusCount todosStatistics__statusCount--todo mb-2 p-3 rounded d-flex justify-content-between align-items-center">
+              TODO<span className="todosStatistics__statusTodo p-2 rounded">{this.getTodosCountByStatus("TODO")}</span>
             </p>
           </Col>
       </div>
