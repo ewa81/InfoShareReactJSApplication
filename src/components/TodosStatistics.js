5980f5e0e9ef63cb30623ac6eec3036abd8a2857
ewa81 <ewa.strzelecka@icloud.com>
2021-06-01 19:29:00 +0200

    Done one.

--- src/components/TodosStatistics.js
+++ src/components/TodosStatistics.js
@@ -5,29 +5,32 @@ import {Row, Col} from "react-bootstrap";
 class TodosStatistics extends Component {
   render() {
     return (
-      <div className="todosStatistics">
-          <Col className="p-0">
+      <Row className="todosStatistics">
+          <Col sm={12}>
             <p className="todosStatistics__statusCount todosStatistics__statusCount--done mb-2 p-3 rounded d-flex justify-content-between align-items-center">
               <span className="todosStatistics__statusLabel">DONE</span>
               <span className="todosStatistics__statusDone p-2 rounded">
-                {this.getTodosCountByStatus("DONE")}</span>
+                {this.getTodosCountByStatus("DONE")}
+              </span>
             </p>
           </Col>
-          <Col className="p-0">
+          <Col sm={12}>
             <p className="todosStatistics__statusCount todosStatistics__statusCount--inprogress mb-2 p-3 rounded d-flex justify-content-between align-items-center">
               <span className="todosStatistics__statusLabel">IN_PROGRESS</span>
               <span className="todosStatistics__statusInprogress p-2 rounded">
-                {this.getTodosCountByStatus("IN_PROGRESS")}</span>
+                {this.getTodosCountByStatus("IN_PROGRESS")}
+              </span>
             </p>
           </Col>
-          <Col className="p-0">
+          <Col sm={12}>
             <p className="todosStatistics__statusCount todosStatistics__statusCount--todo mb-2 p-3 rounded d-flex justify-content-between align-items-center">
               <span className="todosStatistics__statusLabel">TODO</span>
               <span className="todosStatistics__statusTodo p-2 rounded">
-                {this.getTodosCountByStatus("TODO")}</span>
+                {this.getTodosCountByStatus("TODO")}
+              </span>
             </p>
           </Col>
-      </div>
+      </Row>
     );
   }
 
